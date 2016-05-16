a7e6a98cedf08d4d88465053495f078c7c41766f
Tobias Hoffmann <thoffmann.mmt-m2015@fh-salzburg.ac.at>
2016-05-16 17:34:33 +0200

    integrated sfml;

--- modbrain/SourceFiles/main.cpp
+++ modbrain/SourceFiles/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "hello world" << std::endl;
+	std::cout << "modbrain has started!" << std::endl;
+
+	sf::RenderWindow window(sf::VideoMode(512, 512), "modbrain");
+	window.setFramerateLimit(60);
+
+	sf::CircleShape shape(128.0f);
+	shape.setFillColor(sf::Color::Green);
+	shape.setPosition(256, 256);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 
 	return 0;
 }
